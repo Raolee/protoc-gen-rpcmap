2ed428bda2a4d04bbcf1ebaae00838d0551d78d5
RAOL <realaudioonland@gmail.com>
2023-12-19 00:43:29 +0900

    bug : proto version 3에 대한 지원 feature도 작동하도록 수정

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"google.golang.org/protobuf/cmd/protoc-gen-go/internal_gengo"
 	"google.golang.org/protobuf/compiler/protogen"
 	"protoc_gen_rpcmap"
 )
@@ -17,6 +18,10 @@ func main() {
 				return err
 			}
 		}
+
+		// 이걸 달아 줘야... proto version 3를 지원한다.
+		// google.golang.org/protobuf 코드를 더 까보고 알아냄
+		gen.SupportedFeatures = internal_gengo.SupportedFeatures
 		return nil
 	})
 }
